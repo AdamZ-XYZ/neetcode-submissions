7289e92a6d9cc724c36ac8ef3a96a5bbe2003791
Adam Zielinski <adam.zielinski2004@gmail.com>
2026-09-18 14:31:11 +0100

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        #nums.sort()
+        #return nums[int(len(nums)/2)]
+
+        candidate = -1
+        votes = 0
+
+        for current in range(len(nums)):
+            if votes == 0:
+                candidate = nums[current]
+                votes = 1
+            elif  nums[current] == candidate:
+                votes += 1
+            elif nums[current] != candidate:
+                votes -= 1
+        
+        confirm = 0
+        for check in range(len(nums)):
+            if nums[check] == candidate:
+                confirm += 1
+        
+        if confirm > (len(nums)/2):
+            return candidate
+        else: return -1
+                
